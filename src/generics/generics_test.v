0b05fd5a7a7e9694980c251faaf740550445749b
cpx <12119342+carpedil@users.noreply.github.com>
2023-07-16 10:21:24 +0800

    v generics method

--- src/generics/generics_test.v
+++ src/generics/generics_test.v
@@ -11,40 +11,45 @@ fn compare[T](a T, b T) int {
 }
 
 fn test_compare() {
-	println(compare[int](1,2))
-	println(compare[string]("a","b"))
-	println(compare(4,2))
-	println(compare("hello","world"))
+	println(compare[int](1, 2))
+	println(compare[string]('a', 'b'))
+	println(compare(4, 2))
+	println(compare('hello', 'world'))
 }
 
 struct Optional[T] {
-	value T
+	value      T
 	is_defined bool = true
 }
 
 struct Optional2[T] {
-	value T
+	value      T
 	is_defined bool = true
 }
 
-
-fn (o Optional2[T]) map[U](f fn(T) U) Optional2[U] {
+fn (o Optional2[T]) map[U](f fn (T) U) Optional2[U] {
 	if o.is_defined {
 		return Optional2[U]{
 			value: f(o.value)
 		}
 	}
-	return  Optional2[U] {
+	return Optional2[U]{
 		is_defined: false
 	}
 }
 
+fn (o Optional2[T]) or_else(default T) T {
+	if o.is_defined {
+		return o.value
+	}
+	return default
+}
 
 fn test_generics_struct() {
-	op1 := Optional[int] {
-		value :10
+	op1 := Optional[int]{
+		value: 10
 	}
-	op2 := Optional[string] {
+	op2 := Optional[string]{
 		value: 'hello'
 	}
 
@@ -53,19 +58,30 @@ fn test_generics_struct() {
 	assert op2.value.len == 5
 }
 
-
 fn test_map() {
 	op := Optional2[string]{
-        value: '100.5'
-    }
-    println(op.map(fn (str string) int {
-        return str.int()
-    })) // Optional2[int]{value: 100'}
+		value: '100.5'
+	}
+	println(op.map(fn (str string) int {
+		return str.int()
+	})) // Optional2[int]{value: 100'}
 
-    op2 := Optional2[string]{
-        is_defined: false
-    }
-    println(op2.map(fn (str string) int {
-        return str.int()
-    })) // Optional2[int]{is_defined: false}
+	op2 := Optional2[string]{
+		is_defined: false
+	}
+	println(op2.map(fn (str string) int {
+		return str.int()
+	})) // Optional2[int]{is_defined: false}
+}
+
+fn test_or_else() {
+	op := Optional2[string]{
+		is_defined: false
+	}
+	println(op.or_else('default'))
+
+	op2 := Optional2[int]{
+		value: 100
+	}
+	println(op2.or_else(200))
 }
