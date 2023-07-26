1a3cc1438d85d619e2b17315449796c486bf749b
carpedil <cir.yan.dev@outlook.com>
2023-07-26 20:01:05 +0800

    single tcp server message send & receive done

--- src/mnet/client.v
+++ src/mnet/client.v
@@ -4,9 +4,9 @@ import net
 import readline { read_line }
 import log
 
-const address = '10.9.64.28:6612'
+const addresss = ['10.9.64.28:6612', '10.162.138.83:6612']
 
-const address_local = 'localhost:8090'
+const address_local = ['localhost:8090']
 
 fn main() {
 	mut l := log.Log{}
@@ -16,7 +16,7 @@ fn main() {
 	l.log_to_console_too()
 
 	flag := read_line('local test only ? y|n')!
-	addr := if flag.trim_space() == 'y' { address_local } else { address }
+	addr := if flag.trim_space() == 'y' { address_local } else { addresss }
 
 	if flag == 'y' {
 		l.info("you'd better start the local tcp server first!")
@@ -51,28 +51,26 @@ fn main() {
 	}
 }
 
-fn parse_message(msg string) string {
-	msg_len := msg.len
-	msg_len_self := msg_len.str().len
-	return '${msg_len_self}${msg_len}${msg}'
-}
+fn handle_connect_and_send(addrs []string, mut l log.Log) {
+	l.info(' Remote Tcp Server List : ${addrs}...')
+	for addr in addrs {
+		l.info('try to connect Remote Tcp Server @${addr}...')
+		mut conn := connect_to_server(addr, mut l) or { return }
+		defer {
+			conn.close() or { l.error('Failed to close connection: ${err}') }
+		}
+		l.info('connected server @${conn.addr()}')
 
-fn handle_connect_and_send(addr string, mut l log.Log) {
-	mut conn := connect_to_server(addr, mut l) or { return }
-	defer {
-		conn.close() or { l.error('Failed to close connection: ${err}') }
+		// message_list := [
+		// 	'REGCLIENT HDR=FANET,FANET,WIN10-OSF2 DATETIME=20230613093623 ,,,,, IP=10.8.3.125 PORT=6020',
+		// 	'GETCELLID HDR=QUERY_SRV,FANET,WIN10-OSF2,OSFMGR,3.0.0.203,1 DATETIME=20230726150144 CLIENT=WIN10-OSF2',
+		// ]
+		message := 'GETCELLID HDR=QUERY_SRV,FANET,WIN10-OSF2,OSFMGR,3.0.0.203,1 DATETIME=20230726150144 CLIENT=WIN10-OSF2'
+		ms := parse_message(message)
+		l.info('[Send Message]:${ms}')
+
+		send_message(mut conn, ms, mut l)
 	}
-	l.info('connected server @${conn.addr()}')
-
-	// message_list := [
-	// 	'REGCLIENT HDR=FANET,FANET,WIN10-OSF2 DATETIME=20230613093623 ,,,,, IP=10.8.3.125 PORT=6020',
-	// 	'GETCELLID HDR=QUERY_SRV,FANET,WIN10-OSF2,OSFMGR,3.0.0.203,1 DATETIME=20230726150144 CLIENT=WIN10-OSF2',
-	// ]
-	message := 'GETCELLID HDR=QUERY_SRV,FANET,WIN10-OSF2,OSFMGR,3.0.0.203,1 DATETIME=20230726150144 CLIENT=WIN10-OSF2'
-	ms := parse_message(message)
-	l.info('[Send Message]:${ms}')
-
-	send_message(mut conn, ms, mut l)
 }
 
 fn connect_to_server(address string, mut l log.Log) !&net.TcpConn {
@@ -84,4 +82,10 @@ fn connect_to_server(address string, mut l log.Log) !&net.TcpConn {
 
 fn send_message(mut conn net.TcpConn, message string, mut l log.Log) {
 	conn.write_string(message) or { l.error('Failed to send message: ${err}') }
+}
+
+fn parse_message(msg string) string {
+	msg_len := msg.len
+	msg_len_self := msg_len.str().len
+	return '${msg_len_self}${msg_len}${msg}'
 }
