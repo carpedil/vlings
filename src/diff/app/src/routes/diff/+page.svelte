5c859a520796effe529c1ed44a2f2df3e8b85715
carpedil <cir.yan.dev@outlook.com>
2023-07-28 21:24:55 +0800

    gitignore remove diff blocked

---
+++ src/diff/app/src/routes/diff/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    let hello:string = ''
+    onMount(async ()=>{
+        let req = await fetch('http://localhost:8082/diff/hello',{
+            method:'GET'
+
+        })
+        hello = await req.json()
+        console.log(hello)
+    }) 
+</script>
+<div>This is Diff page</div>
+<div>
+    <p class="h1">{hello}</p>
+</div>
