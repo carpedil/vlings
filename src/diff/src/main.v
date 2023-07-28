5e6f152edc292d8c829137da8073f68e0a95b563
cpx <12119342+carpedil@users.noreply.github.com>
2023-07-28 23:08:34 +0800

    comment App.middleware

--- src/diff/src/main.v
+++ src/diff/src/main.v
@@ -19,12 +19,12 @@ const (
 
 struct App {
 	vweb.Context
-pub mut:
-	middlewares map[string][]vweb.Middleware
+// pub mut:
+// 	middlewares map[string][]vweb.Middleware
 }
 
 pub fn (mut app App) before_request() {
-	csrf.protect(mut app.Context, csrf_config)
+	// csrf.protect(mut app.Context, csrf_config)
 	println('[web] before_request: ${app.req.method} ${app.req.url} -> ${app.req.data}')
 }
 
@@ -37,14 +37,14 @@ fn main() {
 	db.close() or { panic(err) }
 
 	mut app := &App{
-		middlewares: {
-			'/': [csrf.middleware(csrf_config)]
-		}
+		// middlewares: {
+		// 	'/': [csrf.middleware(csrf_config)]
+		// }
 	}
 	app.serve_static('/favicon.ico', 'src/assets/favicon.ico')
 	// makes all static files available.
 	os.chdir(os.dir(os.executable()))!
-	// app.handle_static('assets', true)
+	app.handle_static('assets', true)
 	app.mount_static_folder_at(os.resource_abs_path('.'), '/')
 
 	vweb.run(app, port)
@@ -54,10 +54,10 @@ pub fn (mut app App) index() vweb.Result {
 	return $vweb.html()
 }
 
-['/diff']
-pub fn (mut app App) diff() vweb.Result {
-	return app.redirect('/templates/index.html')
-}
+// ['/diff']
+// pub fn (mut app App) diff() vweb.Result {
+// 	return app.redirect('/diff.html')
+// }
 
 ['/diff/hello']
 pub fn (mut app App) hello() vweb.Result {
@@ -76,7 +76,7 @@ pub fn (mut api App) api_save() vweb.Result {
 }
 
 struct ApiData {
-	server_name string //服务名
-	api_content string //	Api格式化后的内容
+	server_name string // 服务名
+	api_content string // Api格式化后的内容
 	test_count  int    // 测试次数，不太准确，但可以作为排序算法的参数。 有利于SNI
 }
