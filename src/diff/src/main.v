89b1508953dbd0520210c4bcaa5e604984be528e
cpx <12119342+carpedil@users.noreply.github.com>
2023-07-30 10:03:51 +0800

    implement  save api data to db

--- src/diff/src/main.v
+++ src/diff/src/main.v
@@ -18,9 +18,8 @@ const (
 )
 
 struct App {
-	vweb.Context
-// pub mut:
-// 	middlewares map[string][]vweb.Middleware
+	vweb.Context // pub mut:
+	// 	middlewares map[string][]vweb.Middleware
 }
 
 pub fn (mut app App) before_request() {
@@ -32,6 +31,7 @@ fn main() {
 	mut db := databases.create_db_connection() or { panic(err) }
 
 	sql db {
+		create table ApiData
 	} or { panic('error on create table: ${err}') }
 
 	db.close() or { panic(err) }
@@ -65,18 +65,36 @@ pub fn (mut app App) hello() vweb.Result {
 }
 
 ['/api/save'; post]
-pub fn (mut api App) api_save() vweb.Result {
-	data := json.decode(ApiData, api.req.data) or {
-		api.set_status(400, '')
-		return api.text('Failed to decode json, error: ${err}')
+pub fn (mut app App) api_save() !vweb.Result {
+	mut db := databases.create_db_connection() or { panic(err) }
+	defer {
+		db.close() or { panic(err) }
+	}
+	data := json.decode(ApiData, app.req.data) or {
+		app.set_status(400, '')
+		return app.text('Failed to decode json, error: ${err}')
 	}
 	//  todo save data to db
+	ad := ApiData{
+		srv_name: data.srv_name
+		api_content: data.api_content
+	}
+	mut insert_error := ''
+	sql db {
+		insert ad into ApiData
+	} or { insert_error = err.msg() }
+	if insert_error != '' {
+		app.set_status(400, '')
+		return app.text('${insert_error}')
+	}
 
-	return api.json('data saved ${data}')
+	return app.json('data saved ${data}')
 }
 
+[table: 'api_data']
 struct ApiData {
-	server_name string // 服务名
-	api_content string // Api格式化后的内容
-	test_count  int    // 测试次数，不太准确，但可以作为排序算法的参数。 有利于SNI
+	id          int    [primary; sql: serial]
+	srv_name    string [nonnull; sql_type: 'TEXT'; unique] // 服务名
+	api_content string [nonnull; sql_type: 'TEXT'] // Api格式化后的内容
+	test_count  int    [default: '0'] // 测试次数，不太准确，但可以作为排序算法的参数。 有利于SNI
 }
