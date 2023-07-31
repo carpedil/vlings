476f44dd0770da658e092111fb48d69a7f422239
carpedil <cir.yan.dev@outlook.com>
2023-07-31 09:07:43 +0800

    add set-up.bat for windows

---
+++ src/diff/app/set-up.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo cd /diff/src
+cd ..\src
+
+echo deleting /_app 
+rmdir /s /q _app
+echo /_app has been deleted!
+
+echo deleting /templates
+rmdir /s /q templates 
+echo /templates has been deleted!
+
+echo deleting favicon.png
+del favicon.png
+echo favicon.png has been deleted!
+
+echo back to /app
+cd ..\app
+
+echo starting run build scripts
+npm run build & move templates ..\src\templates & cd ..\src\templates & move _app ..\  & cd ..\..\app & copy static\favicon.png ..\src\ 
