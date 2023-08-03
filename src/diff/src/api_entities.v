8704e1a4de47ffb337a74ac6994c6806f077f0e5
cpx <12119342+carpedil@users.noreply.github.com>
2023-08-03 18:19:06 +0800

    Update api_entities.v
    add priority & comment to ApiData

--- src/diff/src/api_entities.v
+++ src/diff/src/api_entities.v
@@ -9,6 +9,8 @@ struct ApiData {
 	api_param   string [nonnull; sql_type: 'TEXT']
 	test_count  int    [default: '0'] // 测试次数，不太准确，但可以作为排序算法的参数。 有利于SNI
 	is_inuse    string [default: 'Y']
+	priority 	int    [default: '1']
+	comment     string [default: '';sql_type: 'TEXT']
 }
 
 struct ApiDataDto {
@@ -20,6 +22,8 @@ mut:
 	api_param   []Param
 	test_count  int
 	is_inuse    string
+	priority 	int 
+	comment     string
 }
 
 struct Param {
